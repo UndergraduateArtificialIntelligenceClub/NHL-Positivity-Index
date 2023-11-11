7b1edc53f776ed4dd67b1174c0ca7bfd7c309862
Jacob Winch <winch@ualberta.ca>
2023-11-10 17:26:16 -0700

    Authenticating via oauth

---
+++ authenticating_via_OAuth.py
@@ -0,0 +1,13 @@
+import os 
+import praw
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.environ["CLIENT_ID"]
+SECRET_KEY = os.environ["SECRET_KEY"]
+username = os.environ["username"]
+password = os.environ["password"]
+
+
+
