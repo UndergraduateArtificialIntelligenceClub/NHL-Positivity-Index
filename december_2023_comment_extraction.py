1b6eabeea7fd2bedcc6149a4be6dd1b2a9f1cb53
Brown <subediyukesh@gmail.com>
2024-01-03 13:43:10 -0700

    added function for teams with no-flair and not moderated by hockeymod, changed the main function to combine all lists and then conver to JSON

--- december_2023_comment_extraction.py
+++ december_2023_comment_extraction.py
@@ -5,7 +5,9 @@ from datetime import datetime
 from praw.models import MoreComments
 import json
 from extract_reddit_comments import extract_HockeyMod_comments
-                
+from extract_reddit_comments import extract_user_comments
+
+
 def main():
     load_dotenv()
 
@@ -24,15 +26,27 @@ def main():
         username = username
     )
 
+    #no flair
+    teams_and_users = {
+            "TampaBayLightning": "RandomBoltsFan",
+            "winnipegjets": "DylThaGamer",
+            "AnaheimDucks": "dahooddawg"
+        }
+
     start_date = datetime(2023, 12, 1).date()
     end_date = datetime(2023, 12, 15).date()
 
     all_HockeyMod_comments = extract_HockeyMod_comments(reddit, start_date, end_date)
+    all_no_flair_comments = extract_user_comments(reddit, start_date, end_date, teams_and_users)
+    
+
+    combined_comments = all_HockeyMod_comments.extend(all_no_flair_comments)
+
     json_file_path = 'december_2023_comments.json'
 
-
+   
     with open(json_file_path, 'w') as json_file:
-        json.dump(all_HockeyMod_comments, json_file)
+        json.dump(combined_comments, json_file)
 
 if __name__ == "__main__":
     main()
--- extract_reddit_comments.py
+++ extract_reddit_comments.py
@@ -22,14 +22,49 @@ def extract_HockeyMod_comments(reddit, start_date: datetime, end_date: datetime)
             for comment in submission.comments.list():
                 if isinstance(comment, MoreComments):
                     continue
-            if comment.author != "HockeyMod":
-                comment_info = {
-                    'submission_id': submission.id,  # id of post
-                    'comment_id': comment.id,  # id of comment
-                    'body': comment.body,  # comment text
-                    'score': comment.score,  # number of upvotes
-                    'date': submission_date.strftime('%Y-%m-%d'),  # date comment was posted on 
-                    'subreddit': str(comment.subreddit)  # subreddit comment was posted in 
-                }
-                all_HockeyMod_comments.append(comment_info)
+                if comment.author != "HockeyMod":
+                    comment_info = {
+                        'submission_id': submission.id,  # id of post
+                        'comment_id': comment.id,  # id of comment
+                        'body': comment.body,  # comment text
+                        'score': comment.score,  # number of upvotes
+                        'date': submission_date.strftime('%Y-%m-%d'),  # date comment was posted on 
+                        'subreddit': str(comment.subreddit)  # subreddit comment was posted in 
+                    }
+                    all_HockeyMod_comments.append(comment_info)
     return all_HockeyMod_comments
+
+
+def extract_user_comments(reddit, start_date:datetime, end_date:datetime, teams_and_users) -> list[dict]:
+    """
+    returns a list of dictionaries containing information about each comment on each submission of a specific user in a given time frame
+
+    :param reddit: instance of reddit class in Praw
+    :param start_date: start time of the date range
+    :param end_date: end time of the date range
+    :param teams_and_users: dictionary of teams and users
+    :return: returns a list of dictionaries
+    """
+
+    results = []
+    for subreddit_name, username in teams_and_users.items():
+        subreddit = reddit.subreddit(subreddit_name)
+        user = reddit.redditor(username)
+
+        for submission in user.submissions.new():
+            submission_date = datetime.utcfromtimestamp(submission.created_utc).date()
+            if submission.subreddit == subreddit and start_date <= submission_date <= end_date:
+                for comment in submission.comments.list():
+                    if isinstance(comment, MoreComments):
+                        continue
+                    comment_info ={
+                        'submission_id': submission.id,
+                        'comment_id': comment.id,
+                        'body': comment.body,
+                        'score': comment.score,
+                        'date': submission_date.strftime("%Y-%m-%d"),
+                        'subreddit': subreddit_name
+                    }
+                    results.append(comment_info)
+    return results
+
