2512b4acc651eb845173cb9aa90924388fb76c99
Alexander <abradle1@ualberta.ca>
2024-01-06 15:32:00 -0700

    Finished flaired comment extraction

---
+++ reddit_login.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+import praw
+
+class reddit_initializor():
+    def __init__(self):
+        load_dotenv()
+        
+        CLIENT_ID = os.environ["CLIENT_ID"]
+        SECRET_KEY = os.environ["SECRET_KEY"]
+        username = os.environ["reddit_username"]
+        password = os.environ["reddit_password"]
+
+        self.reddit = praw.Reddit(
+            client_id = CLIENT_ID, 
+            client_secret = SECRET_KEY, 
+            password = password,
+            user_agent = "UAIS", 
+            username = username
+        )
+    
+    def get_reddit(self) -> (praw.Reddit):
+        return self.reddit
