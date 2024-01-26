e6a45bb505b48d6dea9534a34d233386e23a89c3
Jacob Winch <winch@ualberta.ca>
2024-01-26 16:39:00 -0700

    Repo organization

--- code/get_sample.py
+++ code/get_sample.py
@@ -4,7 +4,7 @@ import random
 testing_random_sample = []
 with open('clean_december_2023_comments_body.json', 'r', encoding = 'utf-8') as file:
         data = json.load(file)
-        testing_random_sample = random.sample(data, 100)
+        testing_random_sample = random.sample(data, 20000)
         
-with open('testing_sample_100.json', 'w', encoding='utf-8') as file:
+with open('training_data.json', 'w', encoding='utf-8') as file:
         json.dump(testing_random_sample, file, ensure_ascii=False, indent = 4)
