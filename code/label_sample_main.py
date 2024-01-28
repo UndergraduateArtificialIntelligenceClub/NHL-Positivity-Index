7819c031b5c1078d57dd847ba6c569a8214606fe
Jacob Winch <winch@ualberta.ca>
2024-01-28 14:48:30 -0700

    Added comments for everyone to individually label

--- code/label_sample_main.py
+++ code/label_sample_main.py
@@ -13,10 +13,10 @@ def main():
     
     names = ['Jacob', 'Alex', 'Arden', 'Heiby', 'Tanmay', 'Yukesh']
     
-    users_name = input("What is your name? ").title()
+    users_name = input("What is your first name? ").title()
     while users_name not in names:
         print("Please enter a valid name!")
-        users_name = input("What is your name? ").title()
+        users_name = input("What is your first name? ").title()
     comments_to_label_file_path = f'data/training_data/{users_name}_comments_to_label.json'
     
     
