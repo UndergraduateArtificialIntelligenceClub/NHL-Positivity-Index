e1b003bace72b95414d8b4c5630e381cb2940273
Alexander <abradle1@ualberta.ca>
2024-02-25 15:38:08 -0700

    Finished analyzing model7

--- analyzing_models/confusion_creator.py
+++ analyzing_models/confusion_creator.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.metrics import ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
-
+import math
 # Makes a confusion matrix for each of the models. 
 num_of_models = 4
 model_paths = {
@@ -11,8 +11,9 @@ model_paths = {
                   'lxyuan/distilbert-base-multilingual-cased-sentiments-student',
                   'cardiffnlp/twitter-xlm-roberta-base-sentiment',
                   'mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis',
-                  'ProsusAI/finbert'],
-    "name":["model1","model2","model3","model4","model5","model6"]
+                  'ProsusAI/finbert',
+                  "finiteautomata/bertweet-base-sentiment-analysis"],
+    "name":["model1","model2","model3","model4","model5","model6", "model7"]
     }
 
 model_paths = pd.DataFrame(model_paths)
@@ -22,8 +23,8 @@ for i,model in model_paths.iterrows():
     
     df = pd.read_csv("predictions\\"+model["name"] + ".csv")
     ConfusionMatrixDisplay.from_predictions(df["our label"],df["model label"],cmap = "plasma")
-    score = accuracy_score(df["our label"],df["model label"])
-    title = "Score: "+ str(score) +" Model:\n" + model["model_name"]
+    score = round(accuracy_score(df["our label"],df["model label"]),3)*100
+    title = "Accuracy Score: "+ str(score) +"% Model:\n" + model["model_name"]
     
     
     plt.title(title)
