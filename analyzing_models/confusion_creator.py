ad5a4d8bfaaef455a30106f1e959a4560475727b
Alexander <abradle1@ualberta.ca>
2024-03-01 16:34:23 -0700

    Tested more models

--- analyzing_models/confusion_creator.py
+++ analyzing_models/confusion_creator.py
@@ -14,15 +14,10 @@ model_paths = {
                   'ProsusAI/finbert',
                   "finiteautomata/bertweet-base-sentiment-analysis",
                   "finetuned_bertweet",
-                  "finetuned twitter roberta1",
-                  "finetuned twitter roberta2",
-                  "finetuned twitter roberta3",
-                  "finetuned twitter roberta4",
-                  "finetuned twitter roberta5",
-                  "finetuned twitter roberta6",
-                  "finetuned twitter roberta7",
-                  "finetuned twitter roberta8"],
-    "name":["model1","model2","model3","model4","model5","model6", "model7","model8","model9","model9 (1)","model9 (2)","model9 (3)","model9 (4)","model9 (5)","model9 (6)","model9 (7)"]
+                  "finetuned twitter roberta10",
+                  "finetuned twitter roberta11"],
+    "name":["model1","model2","model3","model4","model5","model6", "model7","model8","model9 (9)","model9 (10)"]
+            # "model9 (10)"]
     }
 
 model_paths = pd.DataFrame(model_paths)
@@ -40,4 +35,4 @@ for i,model in model_paths.iterrows():
     plt.savefig("confusion_matrices\\"+model["name"] + ".jpg", dpi = 2000)
 
 
-    
+    core = round(accuracy_score(df["our label"],df["model label"])*1000)/10
